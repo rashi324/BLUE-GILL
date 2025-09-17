0113a906b8672a73d955e316063b4903676c3a62
ABDUL RASHEED N P <rashiidrashi5@gmail.com>
2025-09-17 14:57:24 +0530

    Update ShopContext.jsx

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -9,7 +9,7 @@ const ShopContextProvider = (props) => {
 
     const currency = '₹';
     const delivery_fee = 10;
-    const backendUrl = "https://blue-gill-backend.onrender.com"
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
     const [search,setSearch] = useState('');
     const [showSearch,setShowSearch] = useState(false);
     const [cartItems, setCartItems] = useState([]);
