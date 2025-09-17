73964b3a138ee193662cb0744ead02376ae17f38
ABDUL RASHEED N P <rashiidrashi5@gmail.com>
2025-09-17 14:50:13 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -4,6 +4,7 @@ import order_icon from './order_icon.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.svg'
 
+export const url = https: "//blue-gill-backend.onrender.com"
 export const assets = {
     logo,
     add_icon,
